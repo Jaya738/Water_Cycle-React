6cd201161d82ac95bd4bf1c9094fdbfd7a32abbf
Your Name <praneethnersu@gmail.com>
2019-03-01 18:43:56 +0530

    added 3 buttons for differnt phases in water cycle

--- src/App.js
+++ src/App.js
@@ -8,17 +8,19 @@ import Cloud from './components/Cloud';
 import Mountain from './components/Mountain';
 import Sun from './components/Sun';
 import Rain from './components/Rain';
+import Button from './components/Button';
 
 class App extends Component {
   render() {
     return (
  <div className='Cycle'>
    
-        
+    <Button/>
+    <Button/>    
   <Container className='Container' >
   <Row>
     <Col sm={4} xs={12} md={4} >
-     
+        
         <Cloud />
         <Mountain />
     </Col>
---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import mount from './images/hill.png'
+import './App.css';
+class Button extends Component {
+    render() {
+        return (
+           <div >     
+             <button className="B1">
+              Phase-1
+            </button>
+            </div>
+        );
+    }
+}
+export default Button;
